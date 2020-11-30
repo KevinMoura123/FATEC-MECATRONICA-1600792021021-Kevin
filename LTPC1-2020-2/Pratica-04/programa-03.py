8e94a3f3a426110abddc823902f21cf2381c8567
KevinMoura123 <72078152+KevinMoura123@users.noreply.github.com>
2020-11-30 20:43:08 -0300

    Create programa-03.py

---
+++ LTPC1-2020-2/Pratica-04/programa-03.py
@@ -0,0 +1,9 @@
+#Verificacao de senha com ELSE
+senha_secreta  =  '123456'
+
+senha  =  input ( 'Informe a sua senha:' )
+
+se  senha  ==  senha_secreta :
+  imprimir ( 'Bem vindo Usuário' )
+mais :
+  imprimir ( 'Acesso negado!' )
