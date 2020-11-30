753b44b6ad79c6ef2d7e5ebee1c2e4f11563f623
KevinMoura123 <72078152+KevinMoura123@users.noreply.github.com>
2020-11-30 20:53:15 -0300

    Create prog05.py

---
+++ LTPC1-2020-2/Pratica-06/prog05.py
@@ -0,0 +1,13 @@
+#Estruturas de decisão
+temperatura_limite  =  float ( entrada ( "Informe uma temperatura limite:" ))
+temperatura_atual  =  flutuante ( entrada ( "Informe a temperatura atual:" ))
+
+se  temperatura_atual  >  temperatura_limite :
+  print ( "Temperatura atual está acima do limite!" )
+elif  temperatura_atual  <  temperatura_limite :
+  print ( 'Temperatura atual abaixo do limite!' )
+mais :
+  print ( 'Temperatura atual está no limite!' )
+
+
+print ( 'Obrigado por utilizar o sistema!' )
