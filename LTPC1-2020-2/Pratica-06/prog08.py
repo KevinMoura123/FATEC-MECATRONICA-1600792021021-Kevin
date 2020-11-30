3e219be13271f2eedc990864dc8135cbe5c68455
KevinMoura123 <72078152+KevinMoura123@users.noreply.github.com>
2020-11-30 20:54:42 -0300

    Create prog08.py

---
+++ LTPC1-2020-2/Pratica-06/prog08.py
@@ -0,0 +1,11 @@
+soma  =  0
+contador  =  0
+
+enquanto  contador  <  3 :
+  numero  =  int ( input ( "Numero:" ))
+  soma  =  soma  +  numero
+  contador  =  contador  +  1
+
+media  =  soma  /  3
+
+imprimir ( "Valor medio:" , mídia )
