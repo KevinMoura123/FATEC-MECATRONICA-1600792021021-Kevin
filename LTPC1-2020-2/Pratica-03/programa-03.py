a8f06fd69fe54ae1dbfd6810a1ee42415534d062
KevinMoura123 <72078152+KevinMoura123@users.noreply.github.com>
2020-11-30 20:34:14 -0300

    Create programa-03.py

---
+++ LTPC1-2020-2/Pratica-03/programa-03.py
@@ -0,0 +1,6 @@
+  
+#variavel numerico inteiro 
+num1  =  int ( input ( 'Informe um valor:' ))
+num2  =  int ( input ( 'Informe outro valor:' ))
+resp  =  num1  +  num2
+imprimir ( 'O resultado é:' , resp )
