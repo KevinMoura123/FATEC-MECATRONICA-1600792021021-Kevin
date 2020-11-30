10d4132ec133884a418769b6bc7028e999cdf962
KevinMoura123 <72078152+KevinMoura123@users.noreply.github.com>
2020-11-30 20:43:34 -0300

    Create programa-04.py

---
+++ LTPC1-2020-2/Pratica-04/programa-04.py
@@ -0,0 +1,9 @@
+#Operadores de divisao (/) e resto da divisao (%)
+number  =  int ( input ( 'Digite um numero:' ))
+
+resto  =  número  %  2
+
+se  resto  ==  0 :
+  imprimir ( 'é par!' )
+mais :
+  imprimir ( 'é ímpar!' )
