ef327e47f8ec46b408d8a86ee702853e53f29ac0
KevinMoura123 <72078152+KevinMoura123@users.noreply.github.com>
2020-11-30 20:52:26 -0300

    Create prog03.py

---
+++ LTPC1-2020-2/Pratica-06/prog03.py
@@ -0,0 +1,12 @@
+# Operações matemáticas
+numero_1  =  float ( input ( "Numero 1:" ))
+numero_2  =  float ( input ( "Numero 2:" ))
+
+imprimir ( "Soma:" , numero_1 + numero_2 )
+imprimir ( "Subtração:" , numero_1 - numero_2 )
+print ( "Multiplicação:" , numero_1 * numero_2 )
+print ( "Divisão:" , numero_1 / numero_2 )
+print ( 'Divisão Inteira:' , numero_1 // numero_2 )
+print ( "Resto da Divisão:" , numero_1 % numero_2 )
+print ( 'Potência:' , numero_1 ** numero_2 )
+imprimir ( "Raiz:" , numero_1 ** ( 1. / numero_2 ))
