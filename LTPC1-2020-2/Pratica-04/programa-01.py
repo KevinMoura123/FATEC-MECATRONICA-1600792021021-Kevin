28f7399c24b206949b47438d61e5957a3ab2f99a
KevinMoura123 <72078152+KevinMoura123@users.noreply.github.com>
2020-11-30 20:41:13 -0300

    Create programa-01.py

---
+++ LTPC1-2020-2/Pratica-04/programa-01.py
@@ -0,0 +1,9 @@
+  
+#Introducao ao IF
+idade  =  int ( input ( 'Informe sua idade:' ))
+
+se  idade  > =  18 :
+  print ( 'Cuidado, agora voce pode ser preso' )
+  estampa ( 'Nao faca besteiras' )
+
+imprimir ( 'Fim do programa' )
