597fbd6bc86702d652748e4d3d439d6a3399da9d
KevinMoura123 <72078152+KevinMoura123@users.noreply.github.com>
2020-11-30 20:51:30 -0300

    Create prog01.py

---
+++ LTPC1-2020-2/Pratica-06/prog01.py
@@ -0,0 +1,7 @@
+imprimir ( 'Ola Mundo' )
+
+#Isso é um comentário - não será necessária
+#A função input () sempre retorna uma string / texto
+nome  =  input ( "Informe seu nome:" )
+
+imprimir ( "Ola" , nome )
