a7656c10a82d56f20804d07ac17dfbaf20c007f4
KevinMoura123 <72078152+KevinMoura123@users.noreply.github.com>
2020-11-30 20:48:28 -0300

    Create exercicio-02.py

---
+++ LTPC1-2020-2/Pratica-05/exercicio-02.py
@@ -0,0 +1,13 @@
+#exercicio 2 - aula 5
+
+lado_a  =  int ( input ( 'Digite o valor do Lado A:' ))
+lado_b  =  int ( entrada ( 'Digite o valor do Lado B:' ))
+lado_c  =  int ( input ( 'Digite o valor do Lado C:' ))
+
+if ( lado_a  >  0 ) e ( lado_b  >  0 ) e ( lado_c  >  0 ):
+  if ( lado_a  +  lado_b  >  lado_c ) e ( lado_a  +  lado_c  >  lado_b ) e ( lado_b  +  lado_c  >  lado_a ):
+    print ( 'É um triângulo!' )
+  mais :
+    imprimir ( 'NÃO é um triângulo!' )
+mais :
+  print ( 'Impossível ser triângulo' )
