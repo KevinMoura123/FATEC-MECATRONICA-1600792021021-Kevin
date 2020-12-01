f1244a1ad15e9bf6c8b724c78d767a62fb86d779
KevinMoura123 <72078152+KevinMoura123@users.noreply.github.com>
2020-11-30 21:20:59 -0300

    Create prog08.py

---
+++ LTPC1-2020-2/Pratica-13/prog08.py
@@ -0,0 +1,8 @@
+numero  =  42
+imprimir ( tipo ( numero ))
+imprimir ( numero )
+imprimir ( numero  +  2 )
+texto  =  str ( numero )
+imprimir ( tipo ( texto ))
+imprimir ( texto )
+imprimir ( texto  +  '2' )
