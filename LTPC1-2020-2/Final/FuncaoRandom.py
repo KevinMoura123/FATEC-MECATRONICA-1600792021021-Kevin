01592f5b5b91a8237252def9e8e4535cc1dc9879
KevinMoura123 <72078152+KevinMoura123@users.noreply.github.com>
2020-11-30 21:24:46 -0300

    Create FuncaoRandom.py

---
+++ LTPC1-2020-2/Final/FuncaoRandom.py
@@ -0,0 +1,12 @@
+#gerar numero aleatorio
+importar  aleatório
+
+#uniform (float)
+valor  =  int ( aleatório . uniforme ( 1000 , 9999 ))
+
+imprimir ( valor )
+
+#randrange (int)
+valor_2  =  aleatório . randrange ( 1000 , 9999 )
+
+imprimir ( valor_2 )
