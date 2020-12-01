c0066ccd751f296acaca48eec597906fbb9db76b
KevinMoura123 <72078152+KevinMoura123@users.noreply.github.com>
2020-11-30 21:17:07 -0300

    Create prog05.py

---
+++ LTPC1-2020-2/Pratica-12/prog05.py
@@ -0,0 +1,13 @@
+nome  =  input ( "Informe seu nome:" )
+valor  =  float ( input ( "Informe um valor:" ))
+numero_conta  =  input ( "Informe o número da conta:" )
+
+validacao  =  input ( "String de validacao:" )
+#escrever nome e numero da conta, separados por ";"
+
+nome_validacao , numero_conta_validacao  =  validacao . dividir ( ';' )
+
+if  nome  ==  nome_validacao  e  numero_conta  ==  numero_conta_validacao :
+  print ( "Dados validados!" )
+mais :
+  print ( "Dados incorretos informados!" )
